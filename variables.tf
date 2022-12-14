e479a03f5c1cca7a496bf1c696c55ae4f5885b1a
David Sissoko <dsissoko@users.noreply.github.com>
2022-12-14 13:52:42 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,17 @@ variable "compartment_id"   {}
 variable "vcn_display_name" {}
 variable "vcn_dns_label"    {}
 
+variable "vnc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vnc_private_subnet_cidr_block" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "vnc_public_subnet_cidr_block" {
+  type    = string
+  default = "10.0.0.0/24"
+}
